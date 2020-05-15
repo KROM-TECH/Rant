790387d7fdac59c16c0de7e3c35f6b9cb0418bfa
kromate <akpananthony33@gmail.com>
2020-05-15 13:00:33 +0100

    design

--- js/display.js
+++ js/display.js
@@ -15,16 +15,18 @@ db.collection("Rants").onSnapshot((snapshot)=>{
     rantRef.getDownloadURL().then(function (url) {
       console.log(data)
       const html = `
-     <li> 
-      <div class="collapsible-header"><i class="material-icons">album</i>${data.title} <span class="blue-text" style="margin-left: 7rem;">Created on: ${data.time}</span></div>
-      <div class="collapsible-body center-align"><span>${data.desc}</span>
-        <br> <br> <br>
-        <audio class="audio" controls="controls">
-          <source type="audio/mpeg"
-            src="${url}">
-        </audio>
-      </div>
-    </li>
+          <li>
+            <div class="collapsible-header"><i class="material-icons">album</i>${data.title} <span class="blue-text"
+                style="margin-left: 7rem;">Created on: ${data.time}</span></div>
+            <div class="collapsible-body center-align"><span>${data.desc}</span>
+              <br> <br> <br>
+              <audio class="audio" controls="controls">
+                <source type="audio/mpeg" src="${url}">
+              </audio>
+              <br>
+              <p class="waves-effect waves-green btn-flat modal-trigger" href= '#commentmodal'>Comment</p>
+            </div>
+          </li>
     `;
       rant.innerHTML += html
     })
