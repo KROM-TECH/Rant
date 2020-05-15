47c0900992ddcdeee83964d16ca2dd6e6b23f844
kromate <akpananthony33@gmail.com>
2020-05-15 01:40:04 +0100

    backend

--- js/display.js
+++ js/display.js
@@ -1,6 +1,33 @@
+const rant = document.querySelector('.collapsible')
+
 db.collection("Rants").onSnapshot((snapshot)=>{
   // console.log(snapshot.docChanges())
   snapshot.docChanges().forEach(change =>{
-    console.log(change, change.doc.data())
+   
+    if (change.type === 'added') {
+      //adding document data to webpage
+      renderRant(change.doc.data())
+    }
   })
+
+  function renderRant(data){
+    var rantRef = storageRef.child('Rants/' + `${data.title}`)
+    rantRef.getDownloadURL().then(function (url) {
+      console.log(data)
+      const html = `
+     <li> 
+      <div class="collapsible-header"><i class="material-icons">album</i>${data.title} <span class="blue-text" style="margin-left: 7rem;">Created on: ${data.time}</span></div>
+      <div class="collapsible-body center-align"><span>${data.desc}</span>
+        <br> <br> <br>
+        <audio class="audio" controls="controls">
+          <source type="audio/mpeg"
+            src="${url}">
+        </audio>
+      </div>
+    </li>
+    `;
+      rant.innerHTML += html
+    })
+    
+  }
 })
